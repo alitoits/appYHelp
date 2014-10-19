d89e6412079874d4774258ae99d0e33335d3144f
Alex Alitoits <aalitoits@exadel.com>
2014-10-19 15:26:19 +0300

    adding sevices;

--- js/controls.js
+++ js/controls.js
@@ -4,25 +4,25 @@
 
 //Pages
 $(function ready() {
-	$('#main-content-block').load("mapPage.html #map-block");
+	$('#main-content-block').load('map-page.html #map-block');
 
 	$('#l0').click(function (eventObject) {
 		eventObject.preventDefault();
-		$('#main-content-block').load("loginPage.html #login-block");
+		$('#main-content-block').load('login-page.html #login-block');
 	});
 
 	$('#l1').click(function (eventObject) {
 		eventObject.preventDefault();
-		$('#main-content-block').load("registerPage.html #register-block");
+		$('#main-content-block').load('register-page.html #register-block');
 	});
 
 	$('#l2').click(function (eventObject) {
 		eventObject.preventDefault();
-		$('#main-content-block').load("putNotePage.html #notes-block");
+		$('#main-content-block').load('put-note-page.html #notes-block');
 	});
 
 	$('#l3').click(function (eventObject) {
 		eventObject.preventDefault();
-		$('#main-content-block').load("settingsPage.html #settings-block");
+		$('#main-content-block').load('settings-page.html #settings-block');
 	});
 });
--- js/services.js
+++ js/services.js
@@ -1,3 +1,57 @@
 'use strict';
 
 //AJAX services
+$(document).ready(function () {
+//login
+
+//logout
+
+//post new note
+	$('#put-note-form').submit(function (event) {
+		if ($('#descr-head').val() === 'correct') {
+			$('#div1').text('Validated...').show();
+			return;
+		}
+
+		event.preventDefault();
+	});
+
+	var newNote = {'advertisement': {
+		'publishedUpTo': 1234567890,
+		'subject': $('#descr-head').val(),
+		'text': $('#descr-note').val(),
+		'latitude': 12.34,
+		'longitude': 45.56
+	}, 'user': {
+		'login': 'alex',
+		'password': 'eewq1111'
+	}
+	};
+
+	$.support.cors = true;
+
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost/api/ad/',
+
+		data: JSON.stringify({ Notes: newNote }),
+		contentType: 'application/json; charset=utf-8',
+
+		xhrFields: {
+			// The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+			// This can be used to set the 'withCredentials' property.
+			// Set the value to 'true' if you'd like to pass cookies to the server.
+			// If this is enabled, your server must respond with the header
+			// 'Access-Control-Allow-Credentials: true'.
+			withCredentials: false
+		},
+
+		dataType: 'json',
+		success: function (data) {
+			console.log(data);
+		},
+		error: function () {
+			console.log('not loading..');
+		}
+	});
+});
